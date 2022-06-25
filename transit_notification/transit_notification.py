02684de5aaed2ccc03c698b24464d8b66e007997
Robert Hennessy <robertghennessy@gmail.com>
2022-06-25 10:29:07 -0700

    transit_notification.py - Added functionality to check that the provided route (line) id is valid - Added functionality to determine the nearest stop to the provided gps coordinates - Added functionality to determine the upcoming vehicles for the given stop - created the message that will eventually be passed to pushover for transmission
    vehicle.py
    - modified the initialization to handle the case that the json is missing some data
    - added columns for delta between current time and expected arrival and departure
    - added functionality to print a delay message

--- transit_notification/transit_notification.py
+++ transit_notification/transit_notification.py
@@ -4,6 +4,9 @@ import dateutil.parser as dp
 import datetime as dt
 import json
 import pandas as pd
+from geopy import distance
+from zoneinfo import ZoneInfo
+import numpy as np
 
 import vehicle as vh
 
@@ -14,43 +17,79 @@ Steps
 3. construct information to send to push notification
 
 """
+# Configuration file
+operator_name = "SF"
+route_name = "14"
+direction_id = "IB"
+# can get the following from google maps
+stop_coordinates = {"lat": 37.759565551910285, "long": -122.41900156132917} # nearest stop = 15557
+local_tz = ZoneInfo('America/Los_Angeles')
+utc = ZoneInfo('UTC')
 
+# Check if operator is valid
 with open("/Users/roberthennessy/Documents/python_project/json/sfmta/operators.json", "r") as file:
     operators_dict = json.load(file)
 
 operators_df = pd.DataFrame(operators_dict)
-
-operator_name = "CT"
-
-# slices dataframe to allow printing of Id and short names
-#print(operators_df[["Id", "ShortName"]])
-
 if operator_name not in operators_df["Id"].values:
-    print("Invalid operator short name")
-if not operators_df[operators_df["Id"] == operator_name]["Monitored"].all():
-    print("Vehicles not monitored")
+    raise ValueError("Invalid operator short name.")
+if not operators_df.loc[operators_df["Id"] == operator_name, "Monitored"].all():
+    raise ValueError("Vehicles not monitored")
 
-with open("/Users/roberthennessy/Documents/python_project/json/caltrain/vehicle_monitoring.json", "r") as file:
-    vehicle_monitoring_dict = json.load(file)
-# check the time of the response
-response_timestamp = dp.parse(vehicle_monitoring_dict["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]
-                              ["ResponseTimestamp"])
-utc_now = dt.datetime.utcnow()
-# TODO - add code to check that the response time is within 2 minutes of current time
+# check if valid route id
+with open("/Users/roberthennessy/Documents/python_project/json/sfmta/lines.json", "r") as file:
+    lines_dict = json.load(file)
+
+routes_df = pd.DataFrame(lines_dict)
+if route_name not in routes_df["Id"].values:
+    raise ValueError("Invalid route id.")
+if not routes_df.loc[routes_df["Id"] == route_name, "Monitored"].all():
+    raise ValueError("Vehicles not monitored for specified route. ")
+
+with open("/Users/roberthennessy/Documents/python_project/json/sfmta/patterns_route_14.json", "r") as file:
+    patterns_dict = json.load(file)
+
+direction_df = pd.DataFrame(patterns_dict['directions'])
+if direction_df not in direction_df["DirectionId"].values:
+    raise ValueError("Invalid route id.")
+
+with open("/Users/roberthennessy/Documents/python_project/json/sfmta/stops_route_14_IB.json", "r") as file:
+    stops_dict = json.load(file)
+
+stops_df = pd.json_normalize(stops_dict["Contents"]["dataObjects"]["ScheduledStopPoint"])
+stops_df['Location.Latitude'] = pd.to_numeric(stops_df['Location.Latitude'], errors='coerce')
+stops_df['Location.Longitude'] = pd.to_numeric(stops_df['Location.Longitude'], errors='coerce')
+stops_df['distance_km'] = stops_df.apply(lambda row: distance.distance(
+    (row["Location.Latitude"], row["Location.Longitude"]), (stop_coordinates["lat"], stop_coordinates["long"])),
+                                axis=1)
+stops_df = stops_df.sort_values(by='distance_km')
+nearest_stop = stops_df.iloc[0]
+nearest_stop_id = nearest_stop["id"]
+nearest_stop_name = nearest_stop["Name"]
+
+with open("/Users/roberthennessy/Documents/python_project/json/sfmta/stop_monitoring_15557.json", "r") as file:
+    stop_monitoring = json.load(file)
+
 vehicles = []
-vehicle_list = vehicle_monitoring_dict["Siri"]["ServiceDelivery"]["VehicleMonitoringDelivery"]["VehicleActivity"]
-print(vehicle_list)
-for vehicle in vehicle_list:
-    vehicles.append(vh.Vehicle(vehicle))
+vehicle_list = stop_monitoring["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"]
+for count, vehicle in enumerate(vehicle_list):
+    vehicles.append(vh.Vehicle(vehicle).output_series())
 
-#delayed_trains_message = [vehicle.delay_message for vehicle in vehicles if vehicle.delay_message is not None]
+stop_monitoring_df = pd.DataFrame(vehicles)
+filter_line_ref = stop_monitoring_df["line_ref"] == route_name
+filter_direction = stop_monitoring_df["direction"] == direction_id
+stop_monitoring_df = stop_monitoring_df.loc[filter_line_ref & filter_direction]
 
-tst_vehicle = vehicles[0]
-print(type(tst_vehicle))
-print(tst_vehicle.__dict__)
+upcoming_vehicles = stop_monitoring_df["stops"].values
+expected_time = []
+for veh in upcoming_vehicles:
+    expected_time.append(veh["ArrivalDelta"].dt.total_seconds().values[0])
 
-with open("/Users/roberthennessy/Documents/python_project/json/caltrain/stop_monitoring.json", "r") as file:
-    stop_monitoring_dict = json.load(file)
+expected_time = np.floor(np.array(expected_time) / 60)
+expected_time = expected_time.astype(int)
 
-tst_stop = stop_monitoring_dict["ServiceDelivery"]["StopMonitoringDelivery"]["MonitoredStopVisit"]
-print(vh.Vehicle(tst_stop[0]).__dict__)
+message_body = f"Route: {route_name}\nStop: {nearest_stop_name}\nStop ID: {nearest_stop_id}\nNext arriavals: "
+message_body = message_body + f"{expected_time[0]} min, {expected_time[1]} min, {expected_time[2]} min"
+
+print(message_body)
+print(len(message_body))
--- transit_notification/vehicle.py
+++ transit_notification/vehicle.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import dateutil.parser as dp
 import numpy as np
+import datetime as dt
+from zoneinfo import ZoneInfo
+
 
 STOP_DF_TIME_COLS = ["AimedArrivalTime", "ExpectedArrivalTime", "AimedDepartureTime", "ExpectedDepartureTime"]
-
+utc = ZoneInfo('UTC')
 
 class Vehicle:
     def __init__(self, input_dict: dict):
         """
-        Create vehicle using data quiered  from 511.org
+        Create vehicle by parsing data queried from 511.org
 
         :param input_dict: dictionary containing information on a vehicle
         :type input_dict: dict
@@ -22,21 +25,22 @@ class Vehicle:
         self.vehicle_journey_ref = monitored_vehicle_journey["FramedVehicleJourneyRef"]["DatedVehicleJourneyRef"]
         self.longitude = float(monitored_vehicle_journey["VehicleLocation"]["Longitude"])
         self.latitude = float(monitored_vehicle_journey["VehicleLocation"]["Latitude"])
-        monitored_call = monitored_vehicle_journey["MonitoredCall"]
-        next_stop = {
-            "StopPointRef": monitored_call["StopPointRef"],
-            "StopPointName": monitored_call["StopPointName"],
-            "AimedArrivalTime": monitored_call["AimedArrivalTime"],
-            "ExpectedArrivalTime": monitored_call["ExpectedArrivalTime"],
-            "AimedDepartureTime": monitored_call["AimedDepartureTime"],
-            "ExpectedDepartureTime": monitored_call["ExpectedDepartureTime"]
-        }
-        if "OnwardCalls" in monitored_vehicle_journey:
-            future_stops = monitored_vehicle_journey["OnwardCalls"]["OnwardCall"]
-            future_stops.insert(0, next_stop)
-        else:
-            future_stops = [next_stop]
-        self.stops = self.parse_stops(pd.DataFrame(future_stops))
+        if "MonitoredCall" in monitored_vehicle_journey:
+            monitored_call = monitored_vehicle_journey["MonitoredCall"]
+            next_stop = {
+                "StopPointRef": monitored_call["StopPointRef"],
+                "StopPointName": monitored_call["StopPointName"],
+                "AimedArrivalTime": monitored_call["AimedArrivalTime"],
+                "ExpectedArrivalTime": monitored_call["ExpectedArrivalTime"],
+                "AimedDepartureTime": monitored_call["AimedDepartureTime"],
+                "ExpectedDepartureTime": monitored_call["ExpectedDepartureTime"]
+            }
+            if "OnwardCalls" in monitored_vehicle_journey:
+                future_stops = monitored_vehicle_journey["OnwardCalls"]["OnwardCall"]
+                future_stops.insert(0, next_stop)
+            else:
+                future_stops = [next_stop]
+            self.stops = self.parse_stops(pd.DataFrame(future_stops))
 
     @staticmethod
     def parse_stops(df: pd.DataFrame) -> pd.DataFrame:
@@ -49,16 +53,27 @@ class Vehicle:
         :return: parsed DataFrame
         :rtype: Pandas DataFrame
         """
-
+        utc_now = dt.datetime.utcnow().replace(tzinfo=utc)
         for col in STOP_DF_TIME_COLS:
             df[col] = pd.to_datetime(df[col], format="%Y-%m-%dT%H:%M:%SZ", utc=True)
         df["DepartureDelay"] = df["ExpectedDepartureTime"] - df["AimedDepartureTime"]
         df["ArrivalDelay"] = df["ExpectedArrivalTime"] - df["AimedArrivalTime"]
+        df["DepartureDelta"] = df["ExpectedDepartureTime"] - utc_now
+        df["ArrivalDelta"] = df["ExpectedArrivalTime"] - utc_now
         return df
+
+    def output_series(self) -> pd.Series:
+        """
+        Convert the object to a Pandas Series.
+
+        :return: Pandas Series
+        :rtype: Pandas Series
+        """
+        return pd.Series(self.__dict__)
 
     def is_vehicle_delayed(self, delay_amount) -> bool:
         """
-        Determine if the vehicle delayed more than amount specified.
+        Determine if the vehicle delayed more than a given number of minutes.
 
         :param delay_amount: Allowed delay in minutes before flagging vehicle as delayed
         :type delay_amount: float
@@ -71,14 +86,62 @@ class Vehicle:
         delayed_arrival = (self.stops["ArrivalDelay"] > delay_criteria).any()
         return delayed_deperature or delayed_arrival
 
+    def delay_message(self, journey_name_switch: int = 1, monitor_arrival: bool = True,  stop_id: str = None) -> str:
+        """
+        Create the delay message for a vehicle.
+
+        :param journey_name_switch: Selects the
+        :type journey_name_switch: int
+
+        :param monitor_arrival: If true, return arrival delays. If false, return departure delays
+        :type monitor_arrival: bool
+
+        :param stop_id: Allowed delay in minutes before flagging vehicle as delayed
+        :type stop_id: str, optional
+
+        :return: Returns true if train delayed
+        :rtype: bool
+        """
+        statement_dict = {}
+        if journey_name_switch == 1:
+            statement_dict["journey_name"] = self.vehicle_journey_ref
+        elif journey_name_switch == 2:
+            statement_dict["journey_name"] = journey_name = self.line_ref
+        else:
+            raise ValueError("journey_name_switch can only have a value of 1 or 2")
+
+        if stop_id:
+            ser = self.stops[self.stops == stop_id]  # TODO - need to convert to series
+        else:
+            ser = self.stops.iloc[0]  # this is already a series
+
+
+
+
+
+
+
+
+
+    def print_time_delta(self, time_delta) -> str:
+        """
+
+        """
+        components = time_delta.components
+        return f"{components.hours:02}:{components.minutes:02}:{components.seconds:02}"
+
 
 
     """
-    For caltrain, delay messages should contain vehicle_journey_ref
+    For caltrain, delay messages should contain vehicle_journey_ref -
     for sfmta, delay message should contain line_ref
 
     vehicle monitoring is good for simple systems - caltrain
     stop monitoring would be good for complex - sfmta
 
+    vehicle monitoring provides more information than stop monitoring.
     data is stored in slightly different formats for vehicle and stop monitoring
     """
+    # want to print something for a given vehicle
+    # with delay, 201 San Mateo 06:30 -> 06:45
+    # no delay, 201 San Mateo 06:30
